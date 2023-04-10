6e2945d26cbeb78f9e4355f7df44e506cf2c8c40
Asia <asiaverner666@gmail.com>
2023-04-10 13:41:18 +0300

    задание 1

---
+++ задание_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Введите трехзначное число: ");
+        int num = int.Parse(Console.ReadLine());
+
+        int secondDigit = (num / 10) % 10;
+
+        Console.WriteLine("Вторая цифра числа: " + secondDigit);
+    }
+}
