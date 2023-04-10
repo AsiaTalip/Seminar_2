1761bb7e35de39037224987e1ee8d706886a6e0a
Asia <asiaverner666@gmail.com>
2023-04-10 13:44:30 +0300

    Задание 2

---
+++ задание_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+        int thirdDigit = GetThirdDigit(number);
+        if (thirdDigit == -1) {
+            Console.WriteLine("Третьей цифры нет");
+        } else {
+            Console.WriteLine("Третья цифра числа {0} равна {1}", number, thirdDigit);
+        }
+    }
+
+    static int GetThirdDigit(int number) {
+        int thirdDigit = -1;
+        if (number >= 100 && number <= 999) {
+            thirdDigit = (number / 100) % 10;
+        }
+        return thirdDigit;
+    }
+}
