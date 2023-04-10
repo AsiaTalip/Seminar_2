41b964ac2e8ca6404937ee4631c7c6fac139353b
Asia <asiaverner666@gmail.com>
2023-04-10 13:47:00 +0300

    Задание 3

---
+++ задание_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите номер дня недели: ");
+        int dayOfWeek = int.Parse(Console.ReadLine());
+
+        if (dayOfWeek == 6 || dayOfWeek == 7)
+        {
+            Console.WriteLine("Да, это выходной день!");
+        }
+        else
+        {
+            Console.WriteLine("Нет, это не выходной день.");
+        }
+
+        Console.ReadKey();
+    }
+}
